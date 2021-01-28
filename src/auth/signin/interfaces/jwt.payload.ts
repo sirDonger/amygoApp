7e6096f19e58dd84d971b8a1c19782b7878df6fe
Paltashka <paltashka50@gmail.com>
2021-01-28 16:50:55 +0200

    merge~~

--- src/auth/signin/interfaces/jwt.payload.ts
+++
@@ -1,3 +0,0 @@
-export interface JwtPayload {
-  readonly userId: string;
-}
--- src/user/interfaces/user.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IUser {
-  readonly id: string;
-  readonly name: string;
-  readonly surname: string;
-  readonly email: string;
-  readonly password: string;
-  readonly phoneNumber: string;
-}
