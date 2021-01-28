9829b5c5cddef17bfe718f970d78a872a4e91b6e
mykola.sirko.work <mykola.sirko.work@gmail.com>
2021-01-28 16:52:58 +0200

    fix dockerfile paths

--- apps/redbrocker/src/redbrocker.controller.ts
+++ apps/redbrocker/src/redbrocker.controller.ts
@@ -7,12 +7,6 @@ export class RedbrockerController {
   private logger = new Logger('RedbrockerController');
   constructor(private readonly redappService: RedbrockerService) {}
 
-  @MessagePattern('add')
-  async accumulate(@Body('data') data: number[]){
-    this.logger.log('Adding ' + data.toString());
-    return this.redappService.accumulate(data);
-  }
-
   @MessagePattern('lastLocation')
   async getLocation(data: object): Promise<object>{
     this.logger.log('Last location: ' + data);
--- apps/redbrocker/src/redbrocker.service.ts
+++ apps/redbrocker/src/redbrocker.service.ts
@@ -2,9 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class RedbrockerService {
-  public accumulate(data: number[]): number {
-    return (data || []).reduce((a, b)=> Number(a) + Number(b));
-  }
 
   public getLocation(data: object){
     const genStringData = JSON.parse(String(data));
